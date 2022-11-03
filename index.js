727a5ad409481ec4e370e0eca786c31c91b9cda6
Karannn23 <93504042+Karannn23@users.noreply.github.com>
2022-11-03 16:56:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var button = document.getElementById("roll-button");
+function rollDice()
+{
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var im1 = "images/dice" + randomNumber1 + ".png";
+  document.querySelectorAll("img")[0].setAttribute("src", im1);
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var im2= "images/dice"+randomNumber2+".png";
+  document.querySelectorAll("img")[1].setAttribute("src", im2);
+
+  if (randomNumber1 > randomNumber2) document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
+  else if (randomNumber2 > randomNumber1) document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  else document.querySelector("h1").innerHTML = "🚩 Draw! 🚩";
+
+}
+button.addEventListener("click",rollDice);
